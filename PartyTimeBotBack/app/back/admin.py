f3b5c9114b13b89475c881b8e59bcf49b2450fc8
Andrey Perevyazko <110488818+Perevyazko1@users.noreply.github.com>
2024-12-01 12:11:17 +0300

    setting admin

--- PartyTimeBotBack/app/back/admin.py
+++ PartyTimeBotBack/app/back/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CustomUser,DateEvent,PartyEvent, Advertising
 
-# Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(DateEvent)
+admin.site.register(PartyEvent)
+admin.site.register(Advertising)
+
+
