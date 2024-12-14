9961c82116fd81da49257db20cc7ce0fe0a28082
Andrey Perevyazko <110488818+Perevyazko1@users.noreply.github.com>
2024-12-14 17:40:22 +0300

    setting cors

--- PartyTimeBotBack/app/app/settings.py
+++ PartyTimeBotBack/app/app/settings.py
@@ -147,3 +147,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ORIGIN_ALLOW_ALL = True
+
+SECURE_CROSS_ORIGIN_OPENER_POLICY = None
