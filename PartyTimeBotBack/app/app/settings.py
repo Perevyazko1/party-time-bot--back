d73e29fda8a33069a6189c57c14a854654c0c49a
Andrey Perevyazko <110488818+Perevyazko1@users.noreply.github.com>
2025-02-12 18:56:56 +0300

    setting get  images

--- PartyTimeBotBack/app/app/settings.py
+++ PartyTimeBotBack/app/app/settings.py
@@ -145,6 +145,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CORS_ALLOW_ALL_ORIGINS = True  # Открывает доступ для всех (можно ограничить конкретными доменами)
 
 
 
